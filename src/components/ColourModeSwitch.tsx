d59e39b505d4057e8602bd79e0904e7b4d273e1e
neofancellu <neo.fancellu@gmail.com>
2023-08-25 16:49:52 +0100

    Built Colour Mode Switch

---
+++ src/components/ColourModeSwitch.tsx
@@ -0,0 +1,14 @@
+import { HStack, Switch, Text, useColorMode } from '@chakra-ui/react'
+
+const ColourModeSwitch = () => {
+    const {toggleColorMode, colorMode} = useColorMode();
+
+  return (
+    <HStack>
+        <Switch colorScheme='green' isChecked={colorMode === 'dark'} onChange={toggleColorMode} />
+        <Text>Dark Mode</Text>
+    </HStack>
+  )
+}
+
+export default ColourModeSwitch
