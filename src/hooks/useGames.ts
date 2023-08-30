dc5b2ccb18f7305f6b8aa25781f6e7bcb75dc9eb
neofancellu <neo.fancellu@gmail.com>
2023-08-30 18:04:29 +0100

    added Platform Icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -3,10 +3,17 @@ import { useState, useEffect } from 'react';
 import apiClient from '../services/api-client';
 import axios from 'axios'
 
+export interface Platform{
+    id: number,
+    name: string,
+    slug: string,
+}
+
 export interface Game {
     id: number,
     name:string,
-    background_image:string
+    background_image:string,
+    parent_platforms:{platform: Platform}[]
 }
 
 interface FetchGamesResponse {
