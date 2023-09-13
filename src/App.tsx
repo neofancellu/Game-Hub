1d6f46a7412d1567122a1d08b9b91730541af3df
neofancellu <neo.fancellu@gmail.com>
2023-09-13 09:37:06 +0100

    added a platform selector

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import GameGrid from './components/GameGrid'
 import GenreList from './components/GenreList'
 import {useState} from "react"
 import { Genre } from './hooks/useGenres'
+import PlatformSelector from './components/PlatformSelector'
 
 function App() {
   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null)
@@ -25,11 +26,12 @@ function App() {
 
       <Show above="lg">
         <GridItem area='aside' paddingX={5}>
-          <GenreList onSelectGenre={(genre) => setSelectedGenre(genre)}/>
+          <GenreList selectGenre={selectedGenre} onSelectGenre={(genre) => setSelectedGenre(genre)}/>
         </GridItem>
       </Show>
 
       <GridItem area='main'  >
+        <PlatformSelector/>
         <GameGrid selectedGenre={selectedGenre}/>
       </GridItem>
 
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -4,9 +4,10 @@ import {List,ListItem,HStack,Image,Text, Spinner, Button} from "@chakra-ui/react
 
 interface Props{
   onSelectGenre: (genre: Genre) => void;
+  selectGenre: Genre | null
 }
 
-const GenreList = ({ onSelectGenre }: Props) => {
+const GenreList = ({selectGenre, onSelectGenre }: Props) => {
     const {data, isLoading, error} = useGenres();
 
   if (error) return null;
@@ -18,7 +19,7 @@ const GenreList = ({ onSelectGenre }: Props) => {
         <ListItem key={genre.id} paddingY="5px">
             <HStack >
                 <Image boxSize="32px" borderRadius={8} src={genre.image_background}/>
-                <Button onClick={() => onSelectGenre(genre)} fontSize='lg' variant="link">{genre.name}</Button>
+                <Button fontWeight={genre.id === selectGenre?.id ? 'bold' : "normal"} onClick={() => onSelectGenre(genre)} fontSize='lg' variant="link">{genre.name}</Button>
             </HStack>
         </ListItem>)}
     </List>
