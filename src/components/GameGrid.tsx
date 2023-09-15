eeb054c8f43ec71e19750a13a13a425421b89cd1
neofancellu <neo.fancellu@gmail.com>
2023-09-15 10:25:03 +0100

    filter game by platforms

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,6 +1,6 @@
 
 import { Text, SimpleGrid } from '@chakra-ui/react';
-import useGames from '../hooks/useGames';
+import useGames, { Platform } from '../hooks/useGames';
 import GameCard from './GameCard';
 import GameCardSkeleton from './GameCardSkeleton';
 import GameCardContainer from './GameCardContainer';
@@ -9,11 +9,12 @@ import { Genre } from '../hooks/useGenres';
 
 interface Props{
   selectedGenre: Genre | null
+  selectedPlatform: Platform | null
 }
 
 
-const GameGrid = ({selectedGenre}: Props) => {
-  const {data,error,isLoading} =useGames(selectedGenre);
+const GameGrid = ({selectedGenre, selectedPlatform}: Props) => {
+  const {data,error,isLoading} =useGames(selectedGenre, selectedPlatform);
   const skeletons = [1,2,3,4,5,6]
 
   return (
