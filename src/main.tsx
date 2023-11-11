d67a31d4a130923f5e940f7e40405ac034360397
neofancellu <neo.fancellu@gmail.com>
2023-11-11 10:43:52 +0000

    setup routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
 import { QueryClient,QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { ChakraProvider,ColorModeScript } from '@chakra-ui/react'
 import theme from './Theme'
+import router from './routes'
+import { RouterProvider } from 'react-router-dom'
 
 const queryClient = new QueryClient()
 
@@ -14,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <RouterProvider router={router}/>
         <ReactQueryDevtools />
       </QueryClientProvider>
     </ChakraProvider>
---
+++ src/pages/GameDetailPage.tsx
@@ -0,0 +1,8 @@
+
+const GameDetailPage = () => {
+  return (
+    <div>GameDetailPage</div>
+  )
+}
+
+export default GameDetailPage
