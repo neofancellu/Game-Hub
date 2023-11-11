be8880a43c439a9afab1be03e85bae54ff4ae32f
neofancellu <neo.fancellu@gmail.com>
2023-11-11 14:05:41 +0000

    build game screenshots

---
+++ src/hooks/useScreenshots.ts
@@ -0,0 +1,16 @@
+import { useQuery } from '@tanstack/react-query'
+import ApiClient from '../services/api-client'
+import { Screenshot } from '../entities/Screenshot'
+
+
+
+const useScreenshots = (gameId: number) => {
+    const apiClient = new ApiClient<Screenshot>(`/games/${gameId}/screenshots`)
+
+    return useQuery({
+        queryKey:['screenshots',gameId],
+        queryFn: apiClient.getAll,
+})}
+
+
+export default useScreenshots; 
